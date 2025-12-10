0d3046168f2d2736824a1a36146d6faedf5616b7
Khazra Ayub <khazra.ayub786@gmail.com>
2025-12-10 16:26:57 +0500

    Fix error

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -2,6 +2,7 @@ import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 type FeatureItem = {
   title: string;
@@ -49,15 +50,28 @@ const FeatureList: FeatureItem[] = [
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
+  let link;
+  if (title === 'Module 1: ROS 2') {
+    link = '/docs/module1-ros2/ros2-overview';
+  } else if (title === 'Module 2: Digital Twin') {
+    link = '/docs/module2-digital-twin/intro-to-digital-twins';
+  } else if (title === 'Module 3: AI-Robot Brain') {
+    link = '/docs/module3-ai-robot-brain/isaac-sim-overview';
+  } else if (title === 'Module 4: VLA') {
+    link = '/docs/module4-vla/llms-for-robotics';
+  }
+
   return (
-    <div className={clsx('col col--3')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+    <div className={clsx(' col col--3 ', styles.featureCard)}>
+      <Link to={link} className={styles.featureLink}>
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+        <div className="text--center padding-horiz--md">
+          <Heading as="h3">{title}</Heading>
+          <p>{description}</p>
+        </div>
+      </Link>
     </div>
   );
 }
